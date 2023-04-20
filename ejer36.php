cbc87137576a8da87e7dd9933d20fdf4b5715ee0
Fran Vera <francisco.vera@telviso.net.ar>
2023-04-20 16:11:45 -0300

    ejer 36 y 37, se muestra la diferencia entre require e include

---
+++ ejer36.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Sitio web</title>
+</head>
+<body>
+    <?php include 'ejer36_1.php';?><!--trae la info de otro archivo-->
+    <?php echo "Hola estoy en la pagina principal";?>
+    
+</body>
+</html>
+
