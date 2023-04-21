f2aef745d2404151d75e06166b588e1089301a67
Fran Vera <francisco.vera@telviso.net.ar>
2023-04-21 10:46:15 -0300

    agrego formulario y columnas tabla para agregar imagenes

--- portafolio/portafolio.php
+++ portafolio/portafolio.php
@@ -1,9 +1,61 @@
 <?php
-    include('cabecera.php')
+include('cabecera.php')
 ?>
 <br>
-Hola fran, aca tenes el portafolio
+<br>
+
+<div class="container">
+    <div class="row">
+
+        <div class="col-md-6">
+            <div class="card">
+                <div class="card-header">
+                    Datos del proyecto
+                </div>
+                <div class="card-body">
+                    <form action="portafolio.php" method="post">
+                        Nombre del proyecto: <input class="form-control" type="text" name="nombre" id="">
+                        <br>
+                        Imagen del proyecto: <input class="form-control" type="text" name="archivo" id="">
+                        <br>
+
+                        <input class="btn btn-success" type="submit" value="Enviar Proyecto">
+                    </form>
+                </div>
+            </div>
+        </div>
+
+        <div class="col-md-6">
+            <div class="table-responsive">
+                <table class="table table-primary">
+                    <thead>
+                        <tr>
+                            <th scope="col">ID</th>
+                            <th scope="col">Nombre</th>
+                            <th scope="col">Imagen</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr class="">
+                            <td scope="row">3</td>
+                            <td>Aplicacion Web</td>
+                            <td>imagen.jpg</td>
+                        </tr>
+                    </tbody>
+                </table>
+            </div>
+        </div>
+
+    </div>
+</div>
+
+
+
+
+
+
+
 
 <?php
-    include "pie.php";
+include "pie.php";
 ?>
