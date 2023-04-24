ae8fe6fb424fb6c1afbcf718a3761c27b412bb8b
Fran Vera <francisco.vera@telviso.net.ar>
2023-04-24 14:23:25 -0300

    se crea la estructura de la pagina, en este caso utilizo bootswatch para el maquetado

---
+++ sitio_web/index.php
@@ -0,0 +1,21 @@
+<?php
+
+include "template/cabecera.php";
+include "template/pie.php";
+
+?>
+
+
+
+<div class="jumbotron">
+    <h1 class="display-3">Bienvenido</h1>
+    <p class="lead">Bienvenido a otro proyecto que estoy armando</p>
+    <hr class="my-2">
+    <p>Mas informacion</p>
+    <p class="lead">
+        <a class="btn btn-primary btn-lg" href="Jumbo action link" role="button">Más información</a>
+    </p>
+</div>
+
+</div>
+</div>
---
+++ sitio_web/nosotros.php
@@ -0,0 +1,13 @@
+<?php
+
+include "template/cabecera.php";
+include "template/pie.php";
+
+?>
+<div class="p-5 mb-4 bg-light rounded-3">
+    <div class="container-fluid py-5">
+      <h1 class="display-5 fw-bold">Nosotros</h1>
+      <p class="col-md-8 fs-4">Soy una persona que esat desarrollando algo</p>
+      <button class="btn btn-primary btn-lg" type="button">No hace nada</button>
+    </div>
+  </div>
