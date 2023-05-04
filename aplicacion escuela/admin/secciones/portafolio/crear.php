32b31936f0bc12b18998af951de332b4f42f5819
Fran Vera <francisco.vera@telviso.net.ar>
2023-05-04 15:24:39 -0300

    se empieza a generar la seccion de portafolio

--- aplicacion escuela/admin/secciones/portafolio/crear.php
+++ aplicacion escuela/admin/secciones/portafolio/crear.php
@@ -1,5 +1,65 @@
-<?php include "../../templates/header.php";?>
+<?php
 
-Crear portafolio
+include("../../bd.php");
 
-<?php include "../../templates/footer.php";?>
+include("../../templates/header.php");
+
+if($_POST){
+    
+}
+
+
+?>
+
+<div class="card">
+    <div class="card-header">
+        Producto del portafolio
+    </div>
+    <div class="card-body">
+        <form action="" enctype="multipart/form-data" method="post">
+
+            <div class="mb-3">
+                <label for="titulo" class="form-label">Titulo:</label>
+                <input type="text" class="form-control" name="titulo" id="titulo" aria-describedby="helpId" placeholder="Titulo">
+            </div>
+            <div class="mb-3">
+                <label for="subtitulo" class="form-label">Subtitulo:</label>
+                <input type="text" class="form-control" name="subtitulo" id="subtitulo" aria-describedby="helpId" placeholder="Subtitulo">
+            </div>
+            <div class="mb-3">
+                <label for="imagen" class="form-label">Imagen:</label>
+                <input type="file" class="form-control" name="imagen" id="imagen" placeholder="Imagen" aria-describedby="fileHelpId">
+            </div>
+            <div class="mb-3">
+                <label for="descripcion" class="form-label">Descripcion:</i></label>
+                <input type="text" class="form-control" name="descripcion" id="descripcion" aria-describedby="helpId" placeholder="Descripcion">
+            </div>
+            <div class="mb-3">
+                <label for="cliente" class="form-label">Cliente:</label>
+                <input type="text" class="form-control" name="cliente" id="cliente" aria-describedby="helpId" placeholder="Cliente">
+            </div>
+            <div class="mb-3">
+                <label for="categoria" class="form-label"><i class="fa fa-caret-square-o-right" aria-hidden="true"></i>Categoria</label>
+                <input type="text" class="form-control" name="categoria" id="categoria" aria-describedby="helpId" placeholder="Categoria">
+            </div>
+            <div class="mb-3">
+                <label for="url" class="form-label">URL:</label>
+                <input type="text" class="form-control" name="url" id="url" aria-describedby="helpId" placeholder="URL del sitio">
+            </div>
+
+            <button type="submit" class="btn btn-success">Agregar</button>
+            <a name="" id="" class="btn btn-primary" href="index.php" role="button">Cancelar</a>
+
+
+        </form>
+    </div>
+    <div class="card-footer text-muted">
+        Footer
+    </div>
+</div>
+
+
+
+
+
+<?php include "../../templates/footer.php"; ?>
--- aplicacion escuela/admin/secciones/portafolio/index.php
+++ aplicacion escuela/admin/secciones/portafolio/index.php
@@ -1,5 +1,58 @@
 <?php 
-include("../../templates/header.php"); 
-include("../../templates/footer.php");
+include ("../../bd.php");
+
+
+$sentencia=$conexion->prepare("SELECT * FROM `tbl_portafolio`");
+$sentencia->execute();
+$lista_portafolio=$sentencia->fetchAll(PDO::FETCH_ASSOC);
+
+
+include("../../templates/header.php");
 ?>
-Listar portafolio
+
+
+
+<div class="card">
+    <div class="card-header">
+    <a name="" id="" class="btn btn-primary" href="crear.php" role="button">Agregar registros</a>
+    </div>
+    <div class="card-body">
+        
+    <div class="table-responsive-sm">
+        <table class="table">
+            <thead>
+                <tr>
+                    <th scope="col">ID</th>
+                    <th scope="col">Titulo</th>
+                    <th scope="col">Subtitulo</th>
+                    <th scope="col">Imagen</th>
+                    <th scope="col">Descripcion</th>
+                    <th scope="col">Cliente</th>
+                    <th scope="col">Categoria</th>
+                    <th scope="col">Url</th>
+                    <td scope="col">Acciones</td>
+                </tr>
+            </thead>
+            <tbody>
+                <tr class="">
+                    <td scope="col">1</td>
+                    <td scope="col">Software de restaurante</td>
+                    <td scope="col">Software para su restaurante a la medida</td>
+                    <td scope="col">imagen.jpg</td>
+                    <td scope="col">Realizado con PHP y MYSQL</td>
+                    <td scope="col">Restaurante Develoteca</td>
+                    <td scope="col">Software</td>
+                    <td scope="col">https://develoteca.com/restaurante</td>
+                    <td scope="col">Editar | Eliminar</td>
+                </tr>
+            </tbody>
+        </table>
+    </div>
+    
+
+    </div>
+</div>
+
+
+
+<?php include("../../templates/footer.php"); ?>
